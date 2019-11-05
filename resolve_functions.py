7f8429160213863fc31ef5be607e8fab49a8f645
Sebastian Chang <schang@Sebastians-Tardis.local>
2019-11-05 17:26:55 -0600

    Added additional functionality to marker stringout batch.  Add new functions for getting just marker colors.  Fixed bugs

--- resolve_functions.py
+++ resolve_functions.py
@@ -24,15 +24,21 @@ mp = project.GetMediaPool()
 sep = '.'
 fps = float(project.GetSetting('timelineFrameRate'))
 
-# Function to get clip colors in active timeline.  Returns an array of used colors called 'active_clip_colors'.
-def get_clip_colors():
+# Function to get clip colors in given timeline.  Returns an array of used colors called 'active_clip_colors'.
+def get_clip_colors(user_timeline):
+    timeline = project.GetTimelineByIndex(user_timeline)
+    check_timeline = project.SetCurrentTimeline(timeline)
     # Gets active timeline from current project
-    active_timeline = project.GetCurrentTimeline() 
+    # active_timeline = project.GetCurrentTimeline() 
+
+    if(not check_timeline):
+        return 'Invalid timeline.'
+
     track_num = 1
     active_clip_colors = []
-    while track_num <= active_timeline.GetTrackCount('video'):
+    while track_num <= timeline.GetTrackCount('video'):
         # Grabs all clips from 'track_num' track from active timeline.
-        video_track_items = active_timeline.GetItemsInTrack('video', track_num)
+        video_track_items = timeline.GetItemsInTrack('video', track_num)
 
         # Checks each individual clip to see if a clip color has been assigned.
         # If clip has a color check to see if that color as already been added to our list.  If not add.
@@ -42,6 +48,23 @@ def get_clip_colors():
                     active_clip_colors.append(video_clip.GetClipColor())
         track_num += 1
     return active_clip_colors
+
+# Function to get the marker colors of given timeline.  Returns an array of used marker colors.
+def get_marker_colors(user_timeline):
+    timeline = project.GetTimelineByIndex(user_timeline)
+    check_timeline = project.SetCurrentTimeline(timeline)
+    # Gets active timeline from current project
+    # active_timeline = project.GetCurrentTimeline() 
+
+    if(not check_timeline):
+        return 'Invalid timeline.'
+
+    markers = timeline.GetMarkers()
+    active_marker_colors = []
+    for marker in markers:
+        if markers[marker]['color'] not in active_marker_colors:
+            active_marker_colors.append(markers[marker]['color'])
+    return active_marker_colors
 
 # Function to return a list of render presets.
 def get_render_presets():
@@ -77,40 +100,63 @@ def add_clip_color_to_render_queue(clip_color, user_preset, render_location):
             video_clip = video_item
             if video_clip.GetClipColor() != 'Default':
                 if clip_color == video_clip.GetClipColor():
-                    file_name = video_clip.GetName().split(sep, 1)[0] + '-' \
+                    filename = video_clip.GetName().split(sep, 1)[0] + '-' \
                     + str(tc.remove_tc_format(tc.frame_to_tc(video_clip.GetStart(), fps)))
                     check_render = project.SetRenderSettings({'MarkIn': video_clip.GetStart(), \
-                    'MarkOut': video_clip.GetEnd() -1, 'TargetDir': render_location, 'CustomName': file_name})
+                    'MarkOut': video_clip.GetEnd() -1, 'TargetDir': render_location, 'CustomName': filename})
                     if(not check_render):
                         return 'An error has occured.'
                     project.AddRenderJob()
         
         track_num += 1
 
-def add_stringout_batch_render_queue(user_timeline, user_preset, render_location):
+# Adds marked portions of a sequence to render queue to the given render location.
+def add_marker_stringout_batch_render_queue(user_timeline, user_preset, user_color, user_suffix, render_location):
     timeline = project.GetTimelineByIndex(user_timeline)
     check_preset = project.LoadRenderPreset(user_preset)
     check_timeline = project.SetCurrentTimeline(timeline)
 
     if(not check_preset):
-        return 'Invalid preset'
+        return 'Invalid preset.'
     if(not check_timeline):
-        return 'Invalid timeline'
+        return 'Invalid timeline.'
     
     timeline_markers = timeline.GetMarkers()
 
     for marker in timeline_markers:
-        if(timeline_markers[marker]['note']):
-            file_name = timeline_markers[marker]['note']
-        else:
-            file_name = timeline.GetName() + '-' + str(tc.remove_tc_format(tc.frame_to_tc(timeline.GetStartFrame() + marker, fps)))
+        if(timeline_markers[marker]['color'] == user_color):
+            filename = timeline_markers[marker]['name']
+            if(user_suffix and not user_suffix.isspace()):
+                filename = filename + '-' + user_suffix
+            else:
+                filename = timeline.GetName() + '-' + str(tc.remove_tc_format(tc.frame_to_tc(timeline.GetStartFrame() + marker, fps)))
 
-        start_time = timeline.GetStartFrame() + marker
-        end_time = start_time + timeline_markers[marker]['duration'] - 1
-        check_render = project.SetRenderSettings({'MarkIn': start_time, 'MarkOut': end_time, 'TargetDir': render_location, 'CustomName': file_name})
-        if(not check_render):
-            return 'An error has occured.'
-        project.AddRenderJob()
+            start_time = timeline.GetStartFrame() + marker
+            end_time = start_time + timeline_markers[marker]['duration'] - 1
+            check_render = project.SetRenderSettings({'MarkIn': start_time, 'MarkOut': end_time, 'TargetDir': render_location, 'CustomName': filename})
+            if(not check_render):
+                return 'An error has occured.'
+            project.AddRenderJob()
+
+# Adds given sequence to render queue with given render preset and render location.
+def add_sequence_batch_render_queue(user_timeline, user_preset, render_location, header_time):
+    timeline = project.GetTimelineByIndex(user_timeline)
+    check_preset = project.LoadRenderPreset(user_preset)
+    check_timeline = project.SetCurrentTimeline(timeline)
+
+    if(not check_preset):
+        return 'Invalid preset.'
+    if(not check_timeline):
+        return 'Invalid timeline.'
+
+    start_time = timeline.GetStartFrame() + header_time
+    end_time = timeline.GetEndFrame() - 1
+    check_render = project.SetRenderSettings({'MarkIn': start_time, 'MarkOut': end_time, 'TargetDir': render_location, 'CustomName': timeline.GetName()})
+
+    if(not check_render):
+        return 'An error has occured.'
+    
+    project.AddRenderJob()
 
 # Returns the number of jobs in render queue.
 def get_number_of_render_jobs():
